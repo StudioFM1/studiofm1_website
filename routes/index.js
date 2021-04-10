d94576cb323d682ea84f628b592dc1c29402d810
Adam Fragkiadakis <adamfrag@gmail.com>
2021-04-10 20:32:24 +0300

    fix bugs in layout

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res,next)=>{
-    res.render('homepage')
+router.get('/', (req, res, next) => {
+    res.render('homepage', { title: 'Studio FM1 105.4' });
 });
 
 module.exports = router
