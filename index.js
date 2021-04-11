22ab129416c95e2eda3e56d08967423f35b9dae3
Adam Fragkiadakis <adamfrag@gmail.com>
2021-04-11 13:58:00 +0300

    add sass

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+/* Dependencies */
+const path = require('path');
 const express = require('express');
-const app = express();
-const port = 80;
 const routes = require('./routes');
-const path= require('path');
+
+const app = express();
 
 /* Set views path and engine */
 app.set('views', path.join(__dirname, 'views'));
@@ -10,8 +11,8 @@ app.set('view engine', 'ejs');
 /* Public folder path */
 app.use(express.static(path.join(__dirname, 'public')));
 
-//ROUTES
+/* Handle routes */
 app.use('/', routes);
 
-
-app.listen(port,()=>console.info('\x1b[34m%s\x1b[0m', `Server listening at http://localhost`));
+const port = 80;
+app.listen(port, () => console.info('\x1b[34m%s\x1b[0m', `Server listening at http://localhost`));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('homepage', { title: 'Studio FM1 105.4' });
-});
+router.get('/', (req, res, next) => res.render('homepage', { title: 'Studio FM1 105.4' }));
 
-module.exports = router
+module.exports = router;
